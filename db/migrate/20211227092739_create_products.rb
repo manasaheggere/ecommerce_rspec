4e6402b33676e89b1826469ddbd6ba06eb4d8b43
manasaheggere <manasaheggere@gmail.com>
2021-12-28 12:43:31 +0530

    initial commit

---
+++ db/migrate/20211227092739_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :products do |t|
+	  t.string :name
+	  t.text :description
+	  t.string :category
+	  t.float :mrp
+	  t.float :special_price
+      t.timestamps
+    end
+  end
+end
